b1ce11c9c31c85edb85803d66e10f34f9da59f67
Abdillahi-A <39855630+Abdillahi-A@users.noreply.github.com>
2019-04-24 15:55:08 +0100

    Update SecretSanta.py

--- SecretSanta.py
+++ SecretSanta.py
@@ -4,33 +4,33 @@ import copy
 def read_csv():
     # reads the csv and returns a list of names and shuffled list of names
     with open('names.csv') as f:
-        lisOfNames = f.read().split('\n')
+        listOfNames = f.read().split('\n')
     
-    shuffledListofNames = copy.copy(lisOfNames)
-    random.shuffle(shuffledListofNames)
+    shuffledListOfNames = copy.copy(listOfNames)
+    random.shuffle(shuffledListOfNames)
 
-    return lisOfNames, shuffledListofNames
+    return listOfNames, shuffledListOfNames
 
-def index_check(lisOfNames, shuffledListofNames):
+def index_check(listOfNames, shuffledListOfNames):
     # checks to make sure that last person to be allocated isn't left with their own name.
-    if lisOfNames[-1] == shuffledListofNames[0]:
-        shuffledListofNames[0], shuffledListofNames[1] = shuffledListofNames[1], shuffledListofNames[0]
+    if listOfNames[-1] == shuffledListOfNames[0]:
+        shuffledListOfNames[0], shuffledListOfNames[1] = shuffledListOfNames[1], shuffledListOfNames[0]
         
-def doAllocations(lisOfNames, shuffledListofNames):
-    index_check(lisOfNames, shuffledListofNames)
-    pairings = []
+def doAllocations(listOfNames, shuffledListOfNames):
+    index_check(listOfNames, shuffledListOfNames)
+    pairingsDict = {}
 
-    for name in lisOfNames:
-        if name == shuffledListofNames[-1]:
-            pairings.append((name, shuffledListofNames.pop(-2)))
+    for name in listOfNames:
+        if name == shuffledListOfNames[-1]:
+            pairingsDict[name] = shuffledListOfNames.pop(-2)
         else:
-            pairings.append((name, shuffledListofNames.pop(-1)))
+            pairingsDict[name] = shuffledListOfNames.pop(-1)
     
-    return pairings
+    return pairingsDict
 
 def main():
-    lisOfNames, shuffledListofNames= read_csv()
-    pairings = doAllocations(lisOfNames, shuffledListofNames)
-    print(pairings)
+    listOfNames, shuffledListOfNames= read_csv()
+    pairingsDict = doAllocations(listOfNames, shuffledListOfNames)
+    print(pairingsDict)
 
 main()
